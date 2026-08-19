00da7bc5b4e9cfd19a35bcef9fdd9245957414fe
Sriram <yannamsriram18@gmail.com>
2026-08-19 21:26:28 +0530

    final commit

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,7 +1,7 @@
 import os
 import sys
 from src.exception import CustomException
-from src.logger import logging
+import src.logger
 import pandas as pd
 
 from sklearn.model_selection import train_test_split
@@ -10,8 +10,8 @@ from dataclasses import dataclass
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
 
-from src.components.model_traning import ModelTrainerConfig
-from src.components.model_traning import ModelTrainer
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
 @dataclass
 class DataIngestionConfig:
     train_data_path: str=os.path.join('artifacts',"train.csv")
@@ -23,23 +23,23 @@ class DataIngestion:
         self.ingestion_config=DataIngestionConfig()
 
     def initiate_data_ingestion(self):
-        logging.info("Entered the data ingestion method or component")
+        src.logger.logging.info("Entered the data ingestion method or component")
         try:
             df=pd.read_csv('notebook\data\stud.csv')
-            logging.info('Read the dataset as dataframe')
+            src.logger.logging.info('Read the dataset as dataframe')
 
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
 
             df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
 
-            logging.info("Train test split initiated")
+            src.logger.logging.info("Train test split initiated")
             train_set,test_set=train_test_split(df,test_size=0.2,random_state=42)
 
             train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
 
             test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
 
-            logging.info("Inmgestion of the data iss completed")
+            src.logger.logging.info("Inmgestion of the data iss completed")
 
             return(
                 self.ingestion_config.train_data_path,
